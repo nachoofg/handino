9b5ddeac69e4ec316e17544e25da9363b2118379
nacho <imfigliuolo@gmail.com>
2023-11-07 11:11:27 -0300

    Update hand_2.ino

--- hand_2.ino
+++ hand_2.ino
@@ -1,23 +1,35 @@
-#include <Servo.h> // esto inclluye la lib
-Servo servo0; // esto capta el servo
-int angulo = 0; // angulo al que dobla 
-int ejex = A1; // eje x del arduino
-int ejey = A2; // eje y del arduino
-int sel = 4; // movimiento del joystick
-int LED = 5; 
-
+#include <Servo.h> 
+Servo viServo, viServo2, viServo3, viServo4; 
+int sensorValue, sensorValue2, sensorValue3, sensorValue4;
+int angle, angle2, angle3, angle4; 
+ 
 void setup() {
-servo0.attach(7); // conexion del servo
-pinMode(sel, INPUT_PULLUP); 
+  Serial.begin(9600); 
+  viServo.attach(6);  
+  viServo2.attach(7);
+  viServo3.attach(8);
+  viServo4.attach(9);
+  delay(1000);
 }
 
 void loop() {
-  angulo =  map( analogRead(A1), 0, 1024, 0, 180);
-        servo0.write(angulo);
-        if ( ! digitalRead(sel))
-              digitalWrite(LED, HIGH);
-        else
-              digitalWrite(LED, LOW);
-        delay(250) ;
-
+  sensorValue = analogRead(A0);
+  sensorValue2 = analogRead(A1);
+  sensorValue3 = analogRead(A2);
+  sensorValue4 = analogRead(A3);
+  
+  angle = map(sensorValue,50,515,200,-113);
+  angle2 = map(sensorValue2,50,515,200,-113);
+  angle3 = map(sensorValue3,50,515,200,-113);
+  angle4 = map(sensorValue4,50,515,200,-113);
+  
+  Serial.println(sensorValue+ sensorValue2+ sensorValue3+ sensorValue4);
+  Serial.print("\t");
+  Serial.println(angle+ angle2+ angle3+ angle4);
+ 
+  viServo.write(angle);
+  viServo2.write(angle2);
+  viServo3.write(angle3);
+  viServo4.write(angle4);
+  delay(200);
 }
